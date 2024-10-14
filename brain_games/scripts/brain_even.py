6a7b929c521dc4459166c64ba1815d8c7af5dc03
Gura-san <gorn89@inbox.ru>
2024-10-14 20:10:42 +0400

    improve while cicle and print answers

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -12,17 +12,23 @@ def is_even(number: int) -> bool:
 def main() -> str:
     counter_of_correct_answers = 0
     welcome_user(NAME)
+    print('Answer "yes" if  number is even, otherwise answer "no".')
     while counter_of_correct_answers < 3:
-        number = randint(0, 100)
-        print('Answer "yes" if  number is even, otherwise answer "no".')
+        number = randint(0, 100)        
         print(f"Question: {number}")
         answer = prompt.string("Your answer: ")
-        if ((is_even(number) and answer == 'yes') or (not is_even(number) and answer == 'no')):
+        if ((is_even(number) and answer == 'yes') or 
+            (not is_even(number) and answer == 'no')):
+            print(f"Your answer: '{'yes' if is_even(number) else 'no'}'")
             print('Correct!')
             counter_of_correct_answers += 1
         else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {'yes' if is_even(number) else 'no'}")
-    print(f"Congratulations, {NAME}")
+            print(f"'{answer}' is wrong answer ;(.",
+                  f"Correct answer was '{'yes' if is_even(number) else 'no'}'")
+            print(f"Let's try again, {NAME}!")
+            break
+    if counter_of_correct_answers == 3:
+        print(f"Congratulations, {NAME}")
 
 
 if __name__ == "__main__":
