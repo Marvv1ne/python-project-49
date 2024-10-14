d6cff060d6162dd7602ca8705026e1698a9c5721
Gura-san <gorn89@inbox.ru>
2024-10-14 19:51:23 +0400

    create a global variable NAME

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+import prompt
 from ..cli import welcome_user
 
+NAME = prompt.string('May I have your name? ')
 
-def main():    
-    welcome_user()
+
+def main():
+    welcome_user(NAME)
 
 
 if __name__ == "__main__":
