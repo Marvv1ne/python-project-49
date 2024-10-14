ec4e14e5fb9b7bc3f03a140eb20270c77c5ea47b
Gura-san <gorn89@inbox.ru>
2024-10-14 19:52:56 +0400

    create a counter_of_correct_answers variable and while cilce

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,7 @@
 import prompt
 from random import randint
 from ..cli import welcome_user
+from .brain_games import NAME
 
 
 def is_even(number: int) -> bool:
@@ -9,15 +10,19 @@ def is_even(number: int) -> bool:
 
 
 def main() -> str:
-    number = randint(0, 100)
-    welcome_user()
-    print('Answer "yes" if  number is even, otherwise answer "no".')
-    print(f"Question: {number}")
-    answer = prompt.string("Your answer: ")
-    if ((is_even(number) and answer == 'yes') or (not is_even(number) and answer == 'no')):
-        print('Correct!')
-    else:
-        print(f"{answer} is wrong answer ;(. Correct answer was {'yes' if is_even(number) else 'no'}")
+    counter_of_correct_answers = 0
+    welcome_user(NAME)
+    while counter_of_correct_answers < 3:
+        number = randint(0, 100)
+        print('Answer "yes" if  number is even, otherwise answer "no".')
+        print(f"Question: {number}")
+        answer = prompt.string("Your answer: ")
+        if ((is_even(number) and answer == 'yes') or (not is_even(number) and answer == 'no')):
+            print('Correct!')
+            counter_of_correct_answers += 1
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {'yes' if is_even(number) else 'no'}")
+    print(f"Congratulations, {NAME}")
 
 
 if __name__ == "__main__":
