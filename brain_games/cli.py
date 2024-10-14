4c173d7848eaa7f878c9a1d1056cf244a0b0222a
Gura-san <gorn89@inbox.ru>
2024-10-14 19:49:48 +0400

    improve welcome_user function

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,5 @@
-import prompt
 
-
-def welcome_user() -> None:
+def welcome_user(name: str) -> None:
+    """The function takes a string with a name and prints a greeting"""
     print("Welcome to the Brain Games")
-    name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
