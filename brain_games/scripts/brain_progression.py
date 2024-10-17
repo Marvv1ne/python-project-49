11f0a18e79314e0fc366828d915a69980439bb2c
Marvv1ne <gorn89@inbox.ru>
2024-10-17 22:25:48 +0400

    add brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+from ..game_logic import game_start
+from .games.prograssion_game import find_lost_element
+
+
+def main() -> str:
+    question = "Find the greatest common divisor of given numbers."
+    game_start(find_lost_element, question)
+
+
+if __name__ == "__main__":
+    main()
