119a9f514dcbdd4fedfc1be921682ea38bea8b1b
Gura-san <gorn89@inbox.ru>
2024-10-21 19:30:18 +0400

    The logic for checking the remainder of division has been moved to a separate function

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -1,13 +1,17 @@
 from random import randint
 from typing import Tuple
 
+def is_divisible(devidend: int,devider: int):
+    """This function return True if numbers
+       are divisible without remainder"""
+    return devidend % devider == 0
 
 def find_gcd() -> Tuple[str, int]:
     """function finds the greatest common divisor of two numbers."""
     number_1 = randint(1, 100)
     number_2 = randint(1, 100)
     expression = f"{number_1} {number_2}"
-    while number_1 % number_2 != 0:
+    while not is_divisible(number_1, number_2):
         number_1, number_2 = number_2, number_1 % number_2
         gcd = number_2
     return expression, gcd
