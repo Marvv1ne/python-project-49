066e1e5e2b970c926e163757af5d3ed52d93c4d4
Gura-san <gorn89@inbox.ru>
2024-10-21 19:33:15 +0400

    Rename is_prime function on is_prime_ game function

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
 from ..game_logic import game_start
-from ..games.is_prime_game import is_prime
+from ..games.is_prime_game import is_prime_game
 
 
 def main():
     question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-    game_start(is_prime, question)
+    game_start(is_prime_game, question)
 
 
 if __name__ == "__main__":
