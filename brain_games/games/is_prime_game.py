6a3c2110d3eea1fa8ea82285d56253b4c646a721
Gura-san <gorn89@inbox.ru>
2024-10-21 19:32:17 +0400

    The logic for checking a prime number has been moved to a separate function

--- brain_games/games/is_prime_game.py
+++ brain_games/games/is_prime_game.py
@@ -2,12 +2,18 @@ from random import randint
 from typing import Tuple
 
 
-def is_prime() -> Tuple[int, str]:
+def is_prime(number: int) -> bool:
+    """This function checks whether
+        a number is prime or not"""
+    for n in range(2, number // 2):
+        if number % n == 0:
+            return False
+    return True
+
+def is_prime_game() -> Tuple[int, str]:
     """function return 'yes' if number is prime or return 'no'"""
     number = randint(0, 100)
-    if number <= 1:
+    if number <= 1 or not is_prime(number):
         return number, "no"
-    for n in range(2, number // 2):
-        if number % 2 == 0:
-            return number, "no"
-    return number, "yes"
+    else:
+        return number, "yes"
