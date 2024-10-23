6ef1b331c36275ce9003cd34bcb8a47f42e402fa
Gura-san <gorn89@inbox.ru>
2024-10-23 19:26:02 +0400

    put name variable into welcome_user function

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,9 @@
+import prompt
 
-def welcome_user(name: str) -> None:
+
+def welcome_user() -> None:
     """The function takes a string with a name and prints a greeting"""
-    print("Welcome to the Brain Games")
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
+    return name
