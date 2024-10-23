9c56c5c6ee0da00e994e9e05c4502b486a7e67d3
Gura-san <gorn89@inbox.ru>
2024-10-23 19:36:01 +0400

    remove extra space in question

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,7 +3,7 @@ from ..games.is_even_game import is_even
 
 
 def main():
-    question = "Answer 'yes' if  number is even, otherwise answer 'no'."
+    question = "Answer 'yes' if number is even, otherwise answer 'no'."
     game_start(is_even, question)
 
 
